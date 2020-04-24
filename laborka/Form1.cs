560fcb40d69cc8256cf1ee8afe53c1e5be3edd66
Filip Jóźwiak <filipjozwiakk@gmail.com>
2020-04-24 16:26:06 +0200

    FILIP_JOZWIAK_LABORKA

--- laborka/Form1.cs
+++ laborka/Form1.cs
@@ -16,5 +16,29 @@ namespace laborka
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           if(textBox1.TextLength > 0)
+            {
+                button1.Text = textBox1.Text;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = checkBox1.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DetailsForm detailsForm = new DetailsForm(textBox1.Text);
+            detailsForm.Show();
+        }
     }
 }
